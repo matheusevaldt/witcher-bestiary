9fec5de1ddb7222a14cfb2eeb42f9b1b056b3860
Matheus Evaldt <evaldtmatheus@gmail.com>
2021-07-02 00:21:32 -0300

    Adding table of contents and more categories to the json file

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import './Categories.css';
 
+import synopsisImage from '../../assets/images/synopsis-image.png'
 import Category from '../Category/Category';
 
 const Categories = ({ categories, setCategories, monsterData, setMonsterData, monsterIsBeingDisplayed, setMonsterIsBeingDisplayed }) => {
@@ -18,7 +19,28 @@ const Categories = ({ categories, setCategories, monsterData, setMonsterData, mo
     return (
 
         <div className='categories-container'>
-            <div className='category'>
+            <div className="synopsis-container">
+                <img src={synopsisImage} alt="The Witcher Bestiary" />
+                <p>
+                    <strong>The Witcher Bestiary</strong> is a vast volume containing information about the living and mythical creatures that have appeared in the world of The Witcher.
+                </p>
+            </div>
+            <div className="toc-container">
+                <div className="toc-header">
+                    <h1>Contents</h1>
+                </div>
+                <div className="toc-list">
+                    {
+                        categories.map(category => {
+                            const id = category.idCategory;
+                            const name = category.nameCategory;
+                            const anchor = `#${id}`;
+                            return <a href={anchor} key={name}><span>{id}.</span> {name}</a>
+                        })
+                    }
+                </div>
+            </div>
+            <div className='categories'>
                 {
                     categories.map(category => (
                         <Category 
